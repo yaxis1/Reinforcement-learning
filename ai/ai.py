87f648406bdd411eb366785d0f50bfd1435d0efc
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-02-24 02:43:49 +0100

    the TORCH

--- ai/ai.py
+++ ai/ai.py
@@ -1,2 +1,12 @@
 #AI for the car
+
 #Importing libraries
+import numpy as np #Working with arrays
+import random #taking random samples
+import os #load and save the model
+import torch #only lib that can handle dynamic graphs
+import torch.nn #neural network
+import torch.nn.functional as F #functions for nn 
+import torch.optim as optim #optimizer for stochastic gradient descent
+import torch.autograd as autograd 
+from torch.autograd import Variable #convergence for tensors to gradient
